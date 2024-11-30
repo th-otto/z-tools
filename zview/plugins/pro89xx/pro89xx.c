6cd1669827a6e863c91d17cb404b1e42aad624bc
Thorsten Otto <admin@tho-otto.de>
2024-11-30 15:05:31 +0100

    Add plugin for PRO89xx/Print Technik

---
+++ zview/plugins/pro89xx/pro89xx.c
@@ -0,0 +1,202 @@
+#define	VERSION	     0x0200
+#define NAME        "PRO89xx (Print-Technik)"
+#define AUTHOR      "Thorsten Otto"
+#define DATE        __DATE__ " " __TIME__
+
+#include "plugin.h"
+#include "zvplugin.h"
+
+/*
+Print-Technik Raw Data    *.HIR
+
+This format is referred to as 'PRO89xx' by other Print-Technik applications. It
+is created by the Print-Technik Professional Digitizer. ImageLab can also
+import this format. The format is documented in the manual.
+
+1 word      file id, $0F0F
+1 word      version [$0001]
+1 word      image width in pixels
+1 word      image height in pixels
+1 word      [$0001]
+--------
+10 bytes    total for header
+
+?           image date:
+image data size = width*height
+1 pixel per byte, values range from 0 = black to 127 = white
+*/
+
+#ifdef PLUGIN_SLB
+
+long __CDECL get_option(zv_int_t which)
+{
+	switch (which)
+	{
+	case OPTION_CAPABILITIES:
+		return CAN_DECODE;
+	case OPTION_EXTENSIONS:
+		return (long) ("HIR\0");
+
+	case INFO_NAME:
+		return (long)NAME;
+	case INFO_VERSION:
+		return VERSION;
+	case INFO_DATETIME:
+		return (long)DATE;
+	case INFO_AUTHOR:
+		return (long)AUTHOR;
+	case INFO_COMPILER:
+		return (long)(COMPILER_VERSION_STRING);
+	}
+	return -ENOSYS;
+}
+#endif
+
+struct file_header {
+	uint16_t magic;
+	uint16_t version;
+	uint16_t width;
+	uint16_t height;
+	uint16_t depth;
+};
+
+/*==================================================================================*
+ * boolean __CDECL reader_init:														*
+ *		Open the file "name", fit the "info" struct. ( see zview.h) and make others	*
+ *		things needed by the decoder.												*
+ *----------------------------------------------------------------------------------*
+ * input:																			*
+ *		name		->	The file to open.											*
+ *		info		->	The IMGINFO struct. to fit.									*
+ *----------------------------------------------------------------------------------*
+ * return:	 																		*
+ *      TRUE if all ok else FALSE.													*
+ *==================================================================================*/
+boolean __CDECL reader_init(const char *name, IMGINFO info)
+{
+	int16_t handle;
+	uint8_t *bmap;
+	struct file_header header;
+	size_t image_size;
+
+	if ((handle = (int16_t) Fopen(name, FO_READ)) < 0)
+	{
+		RETURN_ERROR(EC_Fopen);
+	}
+	
+	if (Fread(handle, sizeof(header), &header) != sizeof(header))
+	{
+		Fclose(handle);
+		RETURN_ERROR(EC_Fread);
+	}
+	if (header.magic != 0xf0f || header.version != 1 || header.depth != 1)
+	{
+		Fclose(handle);
+		RETURN_ERROR(EC_FileId);
+	}
+	image_size = (size_t)header.width * header.height;
+
+	bmap = malloc(image_size);
+	if (bmap == NULL)
+	{
+		Fclose(handle);
+		RETURN_ERROR(EC_Malloc);
+	}
+	if ((size_t)Fread(handle, image_size, bmap) != image_size)
+	{
+		free(bmap);
+		Fclose(handle);
+		RETURN_ERROR(EC_Fread);
+	}
+	Fclose(handle);
+
+	{
+		size_t i;
+		for (i = 0; i < image_size; i++)
+			bmap[i] = bmap[i] * 2 + 1;
+	}
+		
+	info->width = header.width;
+	info->height = header.height;
+	info->planes = 8;
+	info->components = 1;
+	info->colors = 1L << 7;
+	info->real_width = info->width;
+	info->real_height = info->height;
+	info->memory_alloc = TT_RAM;
+	info->page = 1;						/* required - more than 1 = animation */
+	info->orientation = UP_TO_DOWN;
+	info->num_comments = 0;				/* required - disable exif tab */
+	info->_priv_var = 0;				/* y position in bmap */
+	info->_priv_ptr = bmap;
+
+	{
+		int i;
+
+		for (i = 0; i < 256; i++)
+		{
+			info->palette[i].red =
+			info->palette[i].green =
+			info->palette[i].blue = i;
+		}
+	}
+	
+	RETURN_SUCCESS();
+}
+
+
+/*==================================================================================*
+ * boolean __CDECL reader_read:														*
+ *		This function fits the buffer with image data								*
+ *----------------------------------------------------------------------------------*
+ * input:																			*
+ *		buffer		->	The destination buffer.										*
+ *		info		->	The IMGINFO struct. to fit.									*
+ *----------------------------------------------------------------------------------*
+ * return:	 																		*
+ *      TRUE if all ok else FALSE.													*
+ *==================================================================================*/
+boolean __CDECL reader_read(IMGINFO info, uint8_t *buffer)
+{
+	uint8_t *bmap = info->_priv_ptr;
+	memcpy(buffer, &bmap[info->_priv_var], info->width);
+	info->_priv_var += info->width;
+	RETURN_SUCCESS();
+}
+
+
+/*==================================================================================*
+ * boolean __CDECL reader_quit:														*
+ *		This function makes the last job like close the file handler and free the	*
+ *		allocated memory.															*
+ *----------------------------------------------------------------------------------*
+ * input:																			*
+ *		info		->	The IMGINFO struct. to fit.									*
+ *----------------------------------------------------------------------------------*
+ * return:	 																		*
+ *      --																			*
+ *==================================================================================*/
+void __CDECL reader_quit(IMGINFO info)
+{
+	free(info->_priv_ptr);
+	info->_priv_ptr = 0;
+}
+
+
+/*==================================================================================*
+ * boolean __CDECL reader_get_txt													*
+ *		This function , like other function must be always present.					*
+ *		It fills txtdata struct. with the text present in the picture ( if any).	*
+ *----------------------------------------------------------------------------------*
+ * input:																			*
+ *		txtdata		->	The destination text buffer.								*
+ *		info		->	The IMGINFO struct. to fit.									*
+ *----------------------------------------------------------------------------------*
+ * return:	 																		*
+ *      --																			*
+ *==================================================================================*/
+void __CDECL reader_get_txt(IMGINFO info, txt_data *txtdata)
+{
+	(void)info;
+	(void)txtdata;
+}
